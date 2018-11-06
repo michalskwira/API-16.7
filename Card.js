827abeb529abfe73a0a30a38f36c6a858d23340c
Michał Skwira <michalskwira@yahoo.com>
2018-11-06 11:38:38 +0100

    Change files

--- Card.js
+++ Card.js
@@ -1,28 +1,35 @@
 // KLASA KANBAN CARD
-function Card(description) {
+function Card(id, name) {
 	var self = this;
-	
-	this.id = randomString();
-	this.description = description;
+
+	this.id = id;
+	this.name = name || 'no name';
 	this.element = createCard();
 
 	function createCard() {
 		var card = $('<li class="card"></li>');
 		var cardDeleteBtn = $('<button class="btn-delete">x</button>');
 		var cardDescription = $('<p class="card-description"></p>');
-		
-		cardDeleteBtn.click(function(){
+
+		cardDeleteBtn.click(function () {
 			self.removeCard();
 		});
-		
+
 		card.append(cardDeleteBtn);
-		cardDescription.text(self.description);
+		cardDescription.text(self.name);
 		card.append(cardDescription)
 		return card;
 	}
 }
 Card.prototype = {
-	removeCard: function() {
-	  this.element.remove();
+	removeCard: function () {
+		var self = this;
+		$.ajax({
+			url: baseUrl + '/card/' + self.id,
+			method: 'DELETE',
+			success: function (response) {
+				self.element.remove();
+			}
+		});
 	}
 }
--- Column.js
+++ Column.js
@@ -1,8 +1,8 @@
-function Column(name) {
+function Column(id, name) {
 	var self = this;
-	
-	this.id = randomString();
-	this.name = name;
+
+	this.id = id;
+	this.name = name || 'No name';
 	this.element = createColumn();
 
 	function createColumn() {
@@ -12,30 +12,52 @@ function Column(name) {
 		var columnCardList = $('<ul class="card-list"></ul>');
 		var columnDelete = $('<button class="btn-delete">x</button>');
 		var columnAddCard = $('<button class="column-add-card">Dodaj kartę</button>');
-		
+
 		// PODPINANIE ODPOWIEDNICH ZDARZEŃ POD WĘZŁY
-		columnDelete.click(function() {
+		columnDelete.click(function () {
 			self.deleteColumn();
 		});
-		
-		columnAddCard.click(function(event) {
+
+		columnAddCard.click(function (event) {
+			var cardName = prompt('name of the card');
+			if (cardName === null) {
+				return;
+			} 
 			event.preventDefault();
-			self.createCard(new Card(prompt("Wpisz nazwę karty")));
+			$.ajax({
+				url: baseUrl + '/card',
+				method: 'POST',
+				data: {
+					name: cardName,
+					bootcamp_kanban_column_id: self.id
+				},
+				success: function (response) {
+					var card = new Card(response.id, cardName);
+					self.createCard(card);
+				}
+			});
 		});
-			
-			// KONSTRUOWANIE ELEMENTU KOLUMNY
+
+		// KONSTRUOWANIE ELEMENTU KOLUMNY
 		column.append(columnTitle)
 			.append(columnDelete)
 			.append(columnAddCard)
 			.append(columnCardList);
-			return column;
-		}
+		return column;
 	}
+}
 Column.prototype = {
-	createCard: function(card) {
-	  this.element.children('ul').append(card.element);
+	createCard: function (card) {
+		this.element.children('ul').append(card.element);
 	},
-	deleteColumn: function() {
-	  this.element.remove();
+	deleteColumn: function () {
+		var self = this;
+		$.ajax({
+			url: baseUrl + '/column/' + self.id,
+			method: 'DELETE',
+			success: function (response) {
+				self.element.remove();
+			}
+		});
 	}
 };
